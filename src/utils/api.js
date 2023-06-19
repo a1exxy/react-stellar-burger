83653d962a88a715693892bb983a58e4c9016a9a
alex <alex@localhost>
2023-06-19 21:54:18 +0300

    version 2.7

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,17 @@
 // Взаимодействие с API
 
 const options = {method:'GET', redirect:'follow'}
-export default function getApiInfo (url) {
-  return fetch(url, options)
-    .then(res => {
-      if (res.ok) {
-        return res.json()
-      }
-      return Promise.reject(`Ошибка сетевого взаимодействия: ${res.status}`);
-    })
+export default async function getApiInfo (url) {
+  try {
+    return fetch(url, options)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`${response.status}`);
+        } else {
+          return response.json()
+        }
+      })
+  } catch(err) {
+    return Promise.reject(`Ошибка сетевого взаимодействия: ${err.message}`);
+  }
 }
